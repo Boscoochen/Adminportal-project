567b600a955864f18cd3b238644d49e0fe5e2680
Boscoochen <61206389+Boscoochen@users.noreply.github.com>
2021-05-28 16:55:18 -0500

    Add files via upload

---
+++ 4351project/FinanceAdmin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html> 
+<html>
+
+    <head>
+    <title> Finance Admin Dashboard</title>
+    <link rel = "stylesheet" href="Login.css" type="text/css">
+    </head>
+
+    <header>
+        <div class = "div">
+          <img src = " " atl="logo" class = "logo">
+          <h2> Finance Admin Page</h2>
+        </div>
+
+   
+
+
+      <body>
+        <a href="https://uh.edu/nsm/math/graduate/ms-statistics-datascience-outline/">Our Webpage</a> <br>
+        <a href="https://uh.edu/nsm/math/about/">About Us</a> <br>
+      
+          
+        
+      
+      
+        <a href ="logout.php" ><tile><strong>Logout</strong></tile></a>
+      </body>
+
+<style>
+    h2{
+        color: red;
+        
+    }
+</style>
+
+
+</html>
